25dc8a0de9b92ec79af8712bd4f2b3ab02ab01ea
Семен <43268369+PrincePepper@users.noreply.github.com>
2019-05-03 23:44:50 +1000

    Initial commit

--- app/src/main/java/com/example/shool_helper/Fragment_Menu/InformFragment.java
+++ app/src/main/java/com/example/shool_helper/Fragment_Menu/InformFragment.java
@@ -6,12 +6,17 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.shool_helper.R;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class InformFragment extends Fragment {
@@ -21,6 +26,8 @@ public class InformFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         Objects.requireNonNull(getActivity()).setTitle("информатика");
+        ViewPager viewPager = (ViewPager) getActivity().findViewById(R.id.view_pager);
+        setupViewPager(viewPager);
         return inflater.inflate(R.layout.fragment_inform, null);
     }
 
@@ -29,5 +36,46 @@ public class InformFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
 
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+
+        Adapter adapter = new Adapter(getChildFragmentManager());
+
+        adapter.addFragment(new PaymentFragment(), "РАСЧЕТ");
+        //adapter.addFragment(new MyTeamsFixturesFragment(), "РЕШЕНИЕ");
+        viewPager.setAdapter(adapter);
+
+
+
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public Adapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
