d371e1a6a39834ec870aaf2431febb6b6b4fe6ca
Семен <43268369+PrincePepper@users.noreply.github.com>
2019-05-07 00:38:43 +1000

    Initial commit

--- app/src/main/java/com/example/shool_helper/About.java
+++ app/src/main/java/com/example/shool_helper/About.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.text.Spannable;
 import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.widget.TextView;
 
 
