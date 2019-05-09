ead00248d83300e9d7dc25b8489040d5c00a064b
Семен <43268369+PrincePepper@users.noreply.github.com>
2019-05-10 02:10:19 +1000

    Initial commit

--- app/src/main/java/com/example/shool_helper/Fragment_Menu/InfoFragment.java
+++ app/src/main/java/com/example/shool_helper/Fragment_Menu/InfoFragment.java
@@ -8,12 +8,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 
 import com.example.shool_helper.R;
-import com.example.shool_helper.Splash;
 
 import java.util.Objects;
 
@@ -23,20 +19,11 @@ public class InfoFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         Objects.requireNonNull(getActivity()).setTitle("Приветствие");
-        return inflater.inflate(R.layout.fragment_physics, null);
+        return inflater.inflate(R.layout.fragment_info, null);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        if(Splash.picture){
-            ImageView image = Objects.requireNonNull(getActivity()).findViewById(R.id.imageView3);
-            Animation hyperspaceJump = AnimationUtils.loadAnimation(getContext(), R.anim.comboanim);
-            image.startAnimation(hyperspaceJump);
-            Splash.picture=true;
-        }
-
-
-
     }
 }
