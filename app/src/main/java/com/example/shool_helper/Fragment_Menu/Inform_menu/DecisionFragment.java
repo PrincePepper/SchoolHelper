1573e3c5de697e046a8f8722e402feb39f3fd035
Семен <43268369+PrincePepper@users.noreply.github.com>
2019-05-10 03:22:45 +1000

    Initial commit

--- app/src/main/java/com/example/shool_helper/Fragment_Menu/Inform_menu/DecisionFragment.java
+++ app/src/main/java/com/example/shool_helper/Fragment_Menu/Inform_menu/DecisionFragment.java
@@ -40,53 +40,53 @@ public class DecisionFragment extends Fragment {
         TextView decision_textView7=getActivity().findViewById(R.id.decision_textView7);
         if(chooseint_1!=10){
             decision_textView2.setText(getString(R.string.will_give)+strnumber+getString(R.string.number_systems)+chooseint_1+getString(R.string.decimal_number_system));
-            decision_textView2.setTypeface(decision_textView2.getTypeface(), Typeface.BOLD);
+            decision_textView2.setTypeface(null, Typeface.BOLD);
             decision_textView2.setTextColor(Color.BLACK);
             decision_textView2.setTextSize(15);
 
             decision_textView3.setText(strnumber+getString(R.string.arrow)+subtotal);
-            decision_textView3.setTypeface(decision_textView3.getTypeface(), Typeface.ITALIC);
+            decision_textView3.setTypeface(null, Typeface.ITALIC);
             decision_textView3.setTextColor(Color.MAGENTA);
             decision_textView3.setTextSize(20);
 
             decision_textView4.setText(getString(R.string.give_the_integer_part)+subtotal+getString(R.string.in_number_systems)+chooseint_2+getString(R.string.successive_division_by_number)+chooseint_1+getString(R.string.point));
-            decision_textView4.setTypeface(decision_textView4.getTypeface(), Typeface.BOLD);
+            decision_textView4.setTypeface(null, Typeface.BOLD);
             decision_textView4.setTextColor(Color.BLACK);
             decision_textView4.setTextSize(15);
 
-            decision_textView5.setText("Записав полученные цифры в ряд снизу вверх, получим:");
-            decision_textView5.setTypeface(decision_textView5.getTypeface(), Typeface.BOLD);
+            decision_textView5.setText(getString(R.string.writing_the_result));
+            decision_textView5.setTypeface(null, Typeface.BOLD);
             decision_textView5.setTextColor(Color.BLACK);
             decision_textView5.setTextSize(15);
 
             decision_textView6.setText(subtotal+getString(R.string.arrow)+result);
-            decision_textView6.setTypeface(decision_textView6.getTypeface(), Typeface.ITALIC);
+            decision_textView6.setTypeface(null, Typeface.ITALIC);
             decision_textView6.setTextColor(Color.MAGENTA);
             decision_textView6.setTextSize(20);
 
             decision_textView7.setText(resultend);
-            decision_textView7.setTypeface(decision_textView7.getTypeface(), Typeface.BOLD_ITALIC);
-            decision_textView5.setTextColor(Color.RED);
+            decision_textView7.setTypeface(null, Typeface.BOLD_ITALIC);
+            decision_textView7.setTextColor(Color.RED);
             decision_textView7.setTextSize(20);
 
         } else{
             decision_textView2.setText(getString(R.string.give_the_integer_part)+subtotal+getString(R.string.in_number_systems)+chooseint_2+getString(R.string.successive_division_by_number)+chooseint_2+getString(R.string.point));
-            decision_textView2.setTypeface(decision_textView2.getTypeface(), Typeface.BOLD);
+            decision_textView2.setTypeface(null, Typeface.BOLD);
             decision_textView2.setTextColor(Color.BLACK);
             decision_textView2.setTextSize(15);
 
             decision_textView3.setText(getString(R.string.writing_the_result));
-            decision_textView3.setTypeface(decision_textView3.getTypeface(), Typeface.BOLD);
+            decision_textView3.setTypeface(null, Typeface.BOLD);
             decision_textView3.setTextColor(Color.BLACK);
             decision_textView3.setTextSize(15);
 
             decision_textView4.setText(subtotal+"-->"+result);
-            decision_textView4.setTypeface(decision_textView4.getTypeface(), Typeface.ITALIC);
+            decision_textView4.setTypeface(null, Typeface.ITALIC);
             decision_textView4.setTextColor(Color.MAGENTA);
             decision_textView4.setTextSize(20);
 
             decision_textView5.setText(resultend);
-            decision_textView5.setTypeface(decision_textView5.getTypeface(), Typeface.BOLD_ITALIC);
+            decision_textView5.setTypeface(null, Typeface.BOLD_ITALIC);
             decision_textView5.setTextColor(Color.RED);
             decision_textView5.setTextSize(20);
 
