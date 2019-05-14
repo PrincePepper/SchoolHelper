c7aee9853c1beff29f06bf11dffa8557df8f9660
Семен <43268369+PrincePepper@users.noreply.github.com>
2019-05-14 22:30:04 +1000

    Initial commit

--- app/src/main/java/com/example/shool_helper/Splash.java
+++ app/src/main/java/com/example/shool_helper/Splash.java
@@ -1,9 +1,13 @@
 package com.example.shool_helper;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Splash extends AppCompatActivity {
 
@@ -12,14 +16,17 @@ public class Splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_splash);
-        int SPLASH_DISPLAY = 1800;
+        int SPLASH_DISPLAY = 3000;
+        startAnimation();
         new Handler().postDelayed(() -> {
 
             Intent SplashIntent = new Intent(Splash.this, NavigationActivity.class);
             Splash.this.startActivity(SplashIntent);
+
             Splash.this.finish();
 
         }, SPLASH_DISPLAY);
@@ -32,5 +39,11 @@ public class Splash extends AppCompatActivity {
 
         color=false;
         picture=false;
+    }
+    private void startAnimation(){
+
+        ImageView imageView = findViewById(R.id.imageView);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.comboanim);
+        imageView.startAnimation(animation);
     }
 }
