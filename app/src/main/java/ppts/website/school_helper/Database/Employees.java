9cb826aad342152ff5dc2f0c7386e7be23010cc6
semyon <semyon3336@gmail.com>
2020-01-31 22:57:59 +1000

    Initial commit

---
+++ app/src/main/java/ppts/website/school_helper/Database/Employees.java
@@ -0,0 +1,30 @@
+package ppts.website.school_helper.Database;
+
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity
+public class Employees {
+
+    @PrimaryKey(autoGenerate = true)
+    public long id;
+
+    private String formula;
+    String formula_name;
+
+    public Employees(String formula_name, String formula) {
+        this.formula = formula;
+        this.formula_name = formula_name;
+    }
+
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getFormula() { return this.formula; }
+
+    String getFormula_name() { return this.formula_name; }
+
+}
---
+++ app/src/main/java/ppts/website/school_helper/Database/FirebasePhysics.java
@@ -0,0 +1,39 @@
+package ppts.website.school_helper.Database;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+
+public class FirebasePhysics extends AppCompatActivity {
+
+    private DatabaseReference myRef;
+    private static final String TAG = "myLogs";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.d(TAG, "Failed to read value.");
+            }
+        });
+    }
+}
